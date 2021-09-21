ec7ef346c50093c5226d513373f52f46e39e6aec
Stuart Templeton <bvrn007@gmail.com>
2021-09-21 00:24:58 -0700

    first person controller for easier testing

--- GameBoard/GameBoard.gd
+++ GameBoard/GameBoard.gd
@@ -2,16 +2,17 @@ extends Spatial
 
 
 func _ready():
-	GenerateMaze(20,0,5,$Maze.EASY)
+	GenerateMaze(15,0,5,$Maze.EASY)
 
 func GenerateMaze(x = 10, y = 0, z = 10, difficulty = $Maze.MEDIUM):
 	$Maze.connect("maze_generated", self, "ShushLoading")
 	$Maze.connect("maze_progress", self, "LoadingProgress")
 	$Loading.visible = true
 	$Maze.GenerateMaze( Vector3(x,y,z), 0, difficulty )
-	$Camera.transform.origin += Vector3(0,5,0) #pull back y to fit z in frame
-	#$Camera.transform.origin += $Maze.GetMidPoint()
-	$Camera.look_at($Maze.GetMidPoint(),Vector3.UP)
+	$FPS.transform.origin += $Maze.GetMidPoint()
+	#$ViewportContainer/Viewport/Camera.transform.origin += Vector3(0,z * 5,0) #pull back y to fit z in frame
+	#$ViewportContainer/Viewport/Camera.transform.origin += $Maze.GetMidPoint()
+	#$Camera.look_at($Maze.GetMidPoint(),Vector3.UP)
 	
 
 func LoadingProgress(percent, message):
